cda2cc420b83c6790c0dfdb489f94a0db799f6f7
rakesh90084 <90084raky@gmail.com>
2021-07-21 12:08:09 +0530

    dictonaries

---
+++ find.py
@@ -0,0 +1,5 @@
+a=13
+if(a%2==0):
+ print("even")
+else:
+ print("odd")
---
+++ listmembership.py
@@ -0,0 +1,6 @@
+students=["santhu","shika","gulshan","sandhya","arif",45,67,78]
+name=4
+if(name in students):
+ print("present")
+else:
+  print("absent")  
