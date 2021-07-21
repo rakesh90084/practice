745b77465800370f7de48d12a56980c74d3b56c8
rakesh90084 <90084raky@gmail.com>
2021-07-21 14:36:44 +0530

    input

--- list.py
+++ list.py
@@ -2,13 +2,17 @@ names=["rakesh","siddu","raksha","dog"]
 students=["aparna","krupa","arif"]
 state=["tn","kar","mp"]
 id=[10,49,49]
-print(max(id))
-print(min(id))
-print(max(names))
-print(min(names))
-print(tuple(names))
-print(list(names))
-print(cmp(names,state))
+names.append("raaky")
+names.sort()
+names.sort(reverse=True)
+print (names)
+# print(max(id))
+# print(min(id))
+# print(max(names))
+# print(min(names))
+# print(tuple(names))
+# print(list(names))
+# print(cmp(names,state))
 # print(names+students)
 # print(len(names))#finding the length of string
 # print(state*4)
