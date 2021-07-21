643645eea2ac69c48856ee4db2a4e640c51e5b99
rakesh90084 <90084raky@gmail.com>
2021-07-21 14:25:36 +0530

    sets

---
+++ set.py
@@ -0,0 +1,11 @@
+fruit={"orange","apple","mango","apple"}
+juice={"orange"}
+# print(fruit.union(juice))
+# print(fruit.issubset(juice))
+print(fruit.difference(juice))
+# print(fruit&juice)
+# fruit.add("grapes")
+# fruit.remove("orange")
+# fruit.remove("pine apple")
+# fruit.discard("pineapple")
+# print(fruit)
